6a5531a8227717832bd0ef95d77329656acff9c7
TDurr <tylerdurr@gmail.com>
2017-04-27 18:55:49 -0500

    Update FitnessBuddy.java
    Took out some extra commands from the main method that weren't actually doing anything.

--- src/FitnessBuddy.java
+++ src/FitnessBuddy.java
@@ -3,56 +3,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class FitnessBuddy {
-	public static void main (String[] args)
-	{
-		//set up our database connection
-
-		String url = "jdbc:mysql://localhost/FitnessBuddy?user=example&password=password";
-		//SQLite url below
-		//String url = "jdbc:sqlite:/Users/asauppe/Documents/teaching/cs364/sqlite/Shopping.db";
-
-		Connection connect = connect(url);
-
-		if (connect != null)
-			System.out.println("Successful connection!\n");
-		else 
-			System.out.println("Connection failed.\n");
 	
+	public static void main (String[] args)
+	{	
 		GUI g = new GUI();
 		g.makeWindow();
 	}
 
-
-	private static boolean disconnect(Connection connect) 
+	public static boolean disconnect(Connection connect) 
 	{
-		try 
-		{
+		try {
 			connect.close();
 			return true;
 		} 
-		catch (SQLException e) 
-		{
-			// TODO Auto-generated catch block
+		catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
-	private static Connection connect(String url)
+	public static Connection connect(String url)
 	{
 		Connection connect = null;
 
-		try 
-		{
+		try {
 			connect = DriverManager.getConnection(url);
 		} 
-		catch (SQLException e) 
-		{
-			// TODO Auto-generated catch block
+		catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return connect;
 	}
 }
-
-
