84136993c3a13f3262b84c7614aa6b6b18a580b7
littleFenix <binh.qu.nguyen@gmail.com>
2017-04-27 17:34:17 -0500

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -20,8 +20,8 @@ public class GUI extends JFrame implements ActionListener
 	private JPasswordField pass, pass1, pass2;
 	private JTextField day, month, year, calorie, unsaturatedFat, saturatedFat, protein, fiber, carbohydrate,
 	quality, hour, weight, height, bodyFatPercentage, bmi, waistSize, duration, caloriesBurned, activity;
-	
-	public void makeWindow()
+
+	public void logWindow()
 	{
 		//Create a login window
 		logWindow = new JFrame("FITNESS BUDDY");
@@ -29,7 +29,7 @@ public class GUI extends JFrame implements ActionListener
 		logWindow.setLocation(200, 20);
 		logWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		logWindow.setLayout(new GridLayout(8, 8));
-		
+
 		//Create text field and button
 		user = new JTextField("Username", 20);
 		pass = new JPasswordField("Password", 20);
@@ -37,19 +37,22 @@ public class GUI extends JFrame implements ActionListener
 		sign = new Button("NEW USER? SIGN UP!");
 		log.addActionListener(this);
 		sign.addActionListener(this);
-		
+
 		logWindow.add(user);
 		logWindow.add(pass);
 		logWindow.add(log);
 		logWindow.add(sign);
-		
+	}
+
+	public void signWindow()
+	{
 		//Create a sign in window
 		signWindow = new JFrame("SIGN UP");
 		signWindow.setSize(600, 600);
 		signWindow.setLocation(200, 20);
 		signWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		signWindow.setLayout(new GridLayout(8, 8));
-		
+
 		//Create text file for sign up window
 		firstName = new JTextField("First Name", 20);
 		middleName = new JTextField("Middle Name", 20);
@@ -59,13 +62,13 @@ public class GUI extends JFrame implements ActionListener
 		user1 = new JTextField("Username", 20);
 		pass1 = new JPasswordField("Password", 20);
 		pass2 = new JPasswordField("Password", 20);
-		
-		
+
+
 		signBack = new Button("Login Window");
 		signBack.addActionListener(this);
 		submitUser = new Button("Sign In. It's Free!");
 		submitUser.addActionListener(this);
-		
+
 		signWindow.add(firstName);
 		signWindow.add(middleName);
 		signWindow.add(lastName);
@@ -76,14 +79,17 @@ public class GUI extends JFrame implements ActionListener
 		signWindow.add(pass2);
 		signWindow.add(submitUser);
 		signWindow.add(signBack);
-		
+	}
+
+	public void mainMenu()
+	{
 		//Create a main menu window
 		mainMenu = new JFrame("Main Menu");
 		mainMenu.setSize(600, 600);
 		mainMenu.setLocation(200, 20);
 		mainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainMenu.setLayout(new GridLayout(8, 8));
-		
+
 		//All buttons we need
 		dietB = new Button("DIET");
 		sleepB = new Button("SLEEP");
@@ -91,22 +97,25 @@ public class GUI extends JFrame implements ActionListener
 		bodyB = new Button("BODY");
 		submitB = new Button("SUBMIT DATA");
 		back = new Button("LOG OUT");
-		
-		
+
+
 		dietB.addActionListener(this);
 		sleepB.addActionListener(this);
 		actB.addActionListener(this);
 		bodyB.addActionListener(this);
 		submitB.addActionListener(this);
 		back.addActionListener(this);
-		
+
 		mainMenu.add(dietB);
 		mainMenu.add(sleepB);
 		mainMenu.add(actB);
 		mainMenu.add(bodyB);
 		mainMenu.add(submitB);
 		mainMenu.add(back);
-		
+	}
+
+	public void diet()
+	{
 		//Create diet, sleep, activity, body windows
 		diet = new JFrame("DIET");
 		diet.setSize(600, 600);
@@ -118,48 +127,60 @@ public class GUI extends JFrame implements ActionListener
 		dietBack = new Button("MAIN MENU");
 		dietBack.addActionListener(this);
 		diet.add(dietBack);
-		
-		sleep = new JFrame("SLEEP");
-		sleep.setSize(600, 600);
-		sleep.setLocation(200, 20);
-		sleep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		sleep.setLayout(new GridLayout(8, 8));
-		
-		//Sleep function
-		sleepBack = new Button("MAIN MENU");
-		sleepBack.addActionListener(this);
-		sleep.add(sleepBack);
-		
+	}
+
+	public void activity()
+	{
 		//Activity window
 		act = new JFrame("Main Menu");
 		act.setSize(600, 600);
 		act.setLocation(200, 20);
 		act.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		act.setLayout(new GridLayout(8, 8));
-		
+
 		//Act function
 		actBack = new Button("MAIN MENU");
 		actBack.addActionListener(this);
 		act.add(actBack);
-		
+	}
+
+	public void sleep()
+	{
+		sleep = new JFrame("SLEEP");
+		sleep.setSize(600, 600);
+		sleep.setLocation(200, 20);
+		sleep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sleep.setLayout(new GridLayout(8, 8));
+
+		//Sleep function
+		sleepBack = new Button("MAIN MENU");
+		sleepBack.addActionListener(this);
+		sleep.add(sleepBack);
+	}
+
+	public void body()
+	{
 		body = new JFrame("Main Menu");
 		body.setSize(600, 600);
 		body.setLocation(200, 20);
 		body.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		body.setLayout(new GridLayout(8, 8));
-		
+
 		//Body function
 		bodyBack = new Button("MAIN MENU");
 		bodyBack.addActionListener(this);
 		body.add(bodyBack);
-		
+	}
+
+	public void submitWindow()
+	{
 		//Submit window
 		submit = new JFrame("PLEASE ENTER DATA");
 		submit.setSize(600, 600);
 		submit.setLocation(200, 20);
 		submit.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		submit.setLayout(new GridLayout(8, 8));
-		
+
 		//Submit function
 		day  = new JTextField("Day", 2);
 		month  = new JTextField("Month", 2);
@@ -173,14 +194,14 @@ public class GUI extends JFrame implements ActionListener
 		quality  = new JTextField("Quality", 2);
 		hour  = new JTextField("Hours", 20);	
 		weight = new JTextField("Weight", 2);
-	    height = new JTextField("Height", 2);
-	    bodyFatPercentage = new JTextField("Body Fat Percentage", 2);
-	    bmi = new JTextField("BMI", 20);
-	    waistSize = new JTextField("Waist Size", 20);
-	    duration = new JTextField("Duration", 20);
-	    caloriesBurned = new JTextField("Calories Burned", 20);
-	    activity = new JTextField("Name of Activity", 20);
-	   
+		height = new JTextField("Height", 2);
+		bodyFatPercentage = new JTextField("Body Fat Percentage", 2);
+		bmi = new JTextField("BMI", 20);
+		waistSize = new JTextField("Waist Size", 20);
+		duration = new JTextField("Duration", 20);
+		caloriesBurned = new JTextField("Calories Burned", 20);
+		activity = new JTextField("Name of Activity", 20);
+
 		submit.add(day);
 		submit.add(month);
 		submit.add(year);
@@ -201,17 +222,28 @@ public class GUI extends JFrame implements ActionListener
 		submit.add(duration);
 		submit.add(caloriesBurned);
 		submit.add(activity);
-	
+
 		submitBack = new Button("MAIN MENU");
 		submitEnter = new Button("SUBMIT DATA");
-		
+
 		submitBack.addActionListener(this);
 		submitEnter.addActionListener(this);
-		
+
 		submit.add(submitEnter);
 		submit.add(submitBack);
-		
-		
+
+	}
+
+	public void makeWindow()
+	{
+		logWindow();
+		signWindow();
+		mainMenu();
+		diet();
+		activity();
+		sleep();
+		body();
+		submitWindow();
 		logWindow.setVisible(true);
 	}
 
@@ -290,13 +322,13 @@ public class GUI extends JFrame implements ActionListener
 		}
 		if (e.getSource() == submitEnter)
 		{
-			
+
 		}
 		if (e.getSource() == submitUser)
 		{
 			signWindow.setVisible(false);
 			logWindow.setVisible(true);
 		}
-		
+
 	}
 }
